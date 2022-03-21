57dda519d1c9c3961e765be935b9a86a0465b672
嵐杏羽 <82950290+Fader282@users.noreply.github.com>
2022-03-21 12:04:17 +0800

    Update weather_son.py

--- weather_son/weather_son.py
+++ weather_son/weather_son.py
@@ -4,6 +4,7 @@ from hoshino.typing import CQEvent
 import random
 from hoshino.util import FreqLimiter
 from aiocqhttp.message import MessageSegment
+from . import GroupFreqLimiter as freq
 
 sv_help = '''
 看看谁是那位幸运的天弃之子
@@ -11,7 +12,20 @@ sv_help = '''
 
 sv = Service('谁是天弃之子', visible = True, enable_on_default = True, help_ = sv_help)
 
-_flmt = FreqLimiter(30)
+class UsedFlag:
+    def __init__(self, flag = 0):
+        self.flag = 0
+
+    def check(self, gid):
+        if freq.check_reload_group(group_id = gid, _type = 'boolean'):
+            return True
+        else:
+            self.flag = 0
+            return False
+
+emmm = f"{R.img('无语.png').cqcode}"
+_time = 30 # 冷却时间(秒)
+used = UsedFlag()
 
 @sv.on_fullmatch('天弃之子')
 async def weather_son_punish(bot, ev):
@@ -21,12 +35,13 @@ async def weather_son_punish(bot, ev):
     self_info = await bot.get_group_member_info(user_id = sid, group_id = gid, no_cache = True)
     role = self_info['role']
     if role == 'member':
-        await bot.send(ev, '我不是管理员啦')
+        await bot.send(ev, '冰祈不是管理员啦' + emmm)
         return
-    if not _flmt.check(uid):
-        await bot.send(ev, f'唤雷咏唱冷却中...({round(_flmt.left_time(uid))}s)')
+    if freq.check_reload_group(group_id = gid, _type = 'boolean'): # 整个群的冷却 
+        await bot.send(ev, f'唤雷咏唱冷却中...({freq.check_reload_group(gid)}s)')
         return
     await bot.send(ev, f'冰祈咏唱中...')
+    freq.set_reload_group(group_id = gid, _time = _time)
     group_info = await bot.get_group_member_list(group_id = gid)
     member_list = [member['user_id'] for member in group_info]
     member_list.remove(sid)
@@ -42,15 +57,12 @@ async def weather_son_punish(bot, ev):
                 await bot.send(ev, f"一道闪电从天降下，劈中了{MessageSegment.at(son)}!")
             else:
                 await bot.send(ev, f"一道闪电从天降下，但是{son_nickname}幸运地避开了!")
-            _flmt.start_cd(uid)
             return
         if son_role == 'admin':
             await bot.send(ev, f"一道闪电从天降下，但{son_nickname}以管理之力驱散了闪电!")
-            _flmt.start_cd(uid)
             return
         if son_role == 'owner':
             await bot.send(ev, f"一道闪电从天降下，但{son_nickname}以群主之力将闪电送回了天上!")
-            _flmt.start_cd(uid)
             return
     if role == 'owner':
         if random.randrange(101) < 95:
@@ -58,5 +70,4 @@ async def weather_son_punish(bot, ev):
             await bot.send(ev, f"一道闪电从天降下，劈中了{MessageSegment.at(son)}!")
         else:
             await bot.send(ev, f"一道闪电从天降下，但是{MessageSegment.at(son)}幸运地避开了!")
-        _flmt.start_cd(uid)
         return
